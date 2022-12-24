5ea3537005aec9b5b37fd5c5facb656c53d85115
diyor <223@.uz>
2022-12-24 12:16:59 +0500

    master first save

---
+++ main.js
@@ -0,0 +1,18 @@
+var imya = prompt ("Как вас зовут ?")
+console.log("Меня зовут " + imya);
+var vozrast = prompt ("Сколько вам лет?")
+console.log("Мне " + vozrast);
+var primer1 = prompt ("Сколько будет 8+2= ?")
+console.log("Пример 1: 8 + 2 = " + ( 8+2 ) + " Ваш ответ " + primer1);
+var primer2 = prompt ("Сколько будет 9+2= ?")
+console.log("Пример 2: 9 + 2 = " + ( 9+2 ) + " Ваш ответ " + primer2);
+var primer3 = prompt ("Сколько будет 10+2= ?")
+console.log("Пример 3: 10 + 2 = " + ( 10+2 ) + " Ваш ответ " + primer2);
+
+var x = prompt ("Выберите число X")
+var y = prompt ("Выберите число Y")
+var z = prompt ("Выберите число Z")
+console.log('среднее арифметическое числа равен ' + ((x+y+z)/3));
+
+
+
